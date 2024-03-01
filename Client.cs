68a9b6758fb12ee22e0b0bfa5a0a11f06e629e0b
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2024-03-01 18:45:22 +0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class User
+{
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+
+    public string ID { get; set; }
+}
+
+class Client
+{
+    public static async Task Main(string[] args)
+    {
+        TcpClient tcp_client = new TcpClient();
+        await tcp_client.ConnectAsync(IPAddress.Parse("26.86.16.106"), 12345);
+        Console.WriteLine("Connected\n");
+
+
+        var stream = tcp_client.GetStream();
+
+        while (true)
+        {
+            User user = new User();
+
+            Console.Write("login: ");
+            user.Login = Console.ReadLine();
+
+            Console.Write("password: ");
+            user.Password = Console.ReadLine();
+
+            Console.Write("id: ");
+            user.ID = Console.ReadLine();
+
+            Console.WriteLine();
+
+
+            string str = Newtonsoft.Json.JsonConvert.SerializeObject(user);
+            str += '\0';
+
+
+            if (str != null)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(str);
+
+                await stream.WriteAsync(bytes);
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class User
+{
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+
+    public string ID { get; set; }
+}
+
+class Server
+{
+    public static async Task Main(string[] args)
+    {
+        TcpListener tcp_listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 12345);
+        tcp_listener.Start();
+
+        Console.WriteLine("Server started...");
+
+        TcpClient tcp_client = await tcp_listener.AcceptTcpClientAsync();
+
+        await Console.Out.WriteLineAsync("Client started...\n");
+
+
+        var stream = tcp_client.GetStream();
+        List<byte> bytes = new List<byte>();
+
+        while (true)
+        {
+            int bytes_read = 0;
+
+            while ((bytes_read = stream.ReadByte()) != '\0')
+            {
+                bytes.Add((byte)bytes_read);
+            }
+
+            string str = Encoding.UTF8.GetString(bytes.ToArray());
+
+            User user = Newtonsoft.Json.JsonConvert.DeserializeObject<User>(str);
+
+            await Console.Out.WriteLineAsync($"login: {user.Login}\npassword: {user.Password}\nid: {user.ID}\n");
+
+            bytes.Clear();
+        }
+    }
+}
